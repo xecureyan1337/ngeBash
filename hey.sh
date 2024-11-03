b731dea45035bd969230e2f7d4adc74437af3a2f
xecureyan1337 <1227050084@student.uinsgd.ac.id>
2024-11-03 20:17:04 +0700

    cli arguments

--- hey.sh
+++ hey.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Shebang tells the shell to execute it via bash shell.
 
-echo -e "Today is `date`\n"
-
-read -p "What are you looking for => " files
-
-echo -e "\nHere your files => $files" 
+# Command line arguments
+echo "Hello, $1"
